ac9f77720e45313b99af160bb5fff8b50040e119
Jerin John <jerin.j@somaiya.edu>
2017-08-18 01:19:25 +0530

    Splash Screen final.

--- app/src/main/java/com/example/mitrjain/homeautomation/SplashScreenActivity.java
+++ app/src/main/java/com/example/mitrjain/homeautomation/SplashScreenActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Window;
+import android.view.WindowManager;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
@@ -11,6 +12,7 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash_screen);
 
         Thread timerThread = new Thread() {

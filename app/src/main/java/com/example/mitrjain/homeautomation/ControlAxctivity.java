8fc67cbecaf8f286df66ac737f0e887d43cf2612
Jerin John <jerin.j@somaiya.edu>
2017-09-14 00:42:03 +0530

    Icon Animations Final.

--- app/src/main/java/com/example/mitrjain/homeautomation/ControlAxctivity.java
+++ app/src/main/java/com/example/mitrjain/homeautomation/ControlAxctivity.java
@@ -1,7 +1,6 @@
 package com.example.mitrjain.homeautomation;
 
 import android.os.Bundle;
-import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;

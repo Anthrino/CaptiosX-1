274c986c2157178b7bd1525ed82c4a90f91aae35
Jerin John <jerin.j@somaiya.edu>
2017-09-14 00:41:36 +0530

    Icon Animations Final.

--- app/src/main/java/com/example/mitrjain/homeautomation/BedroomActivity.java
+++ app/src/main/java/com/example/mitrjain/homeautomation/BedroomActivity.java
@@ -5,13 +5,10 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.Switch;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
 
 /**
  * Created by Johns on 9/1/2017.
@@ -19,45 +16,43 @@ import org.w3c.dom.Text;
 
 public class BedroomActivity extends AppCompatActivity {
 
-    Switch master,fans,lights,tv,ac;
+    Switch master, fans, lights, tv, ac;
     SeekBar ac_seek_bar;
-    Integer switchCount=0;
+    Integer switchCount = 0;
     TextView temp_text;
+    ImageView tv_icon, light_icon, ac_icon;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.bedroom_fragment);
-        master = (Switch)findViewById(R.id.master_switch);
-        fans = (Switch)findViewById(R.id.fans_switch);
-        lights = (Switch)findViewById(R.id.lights_switch);
-        tv = (Switch)findViewById(R.id.tv_switch);
-        ac=(Switch)findViewById(R.id.ac_switch);
-        ac_seek_bar=(SeekBar)findViewById(R.id.ac_temp_ctrl);
-        temp_text=(TextView)findViewById(R.id.temp_text);
+        master = (Switch) findViewById(R.id.master_switch);
+        fans = (Switch) findViewById(R.id.fans_switch);
+        lights = (Switch) findViewById(R.id.lights_switch);
+        tv = (Switch) findViewById(R.id.tv_switch);
+        ac = (Switch) findViewById(R.id.ac_switch);
+        ac_seek_bar = (SeekBar) findViewById(R.id.ac_temp_ctrl);
+        temp_text = (TextView) findViewById(R.id.temp_text);
+        tv_icon = (ImageView) findViewById(R.id.tv_image);
+        light_icon = (ImageView) findViewById(R.id.lights_image);
+        ac_icon = (ImageView) findViewById(R.id.ac_icon);
         ac_seek_bar.setBackgroundColor(Color.parseColor("#b2ebf2"));
         temp_text.setBackgroundColor(Color.parseColor("#b2ebf2"));
         temp_text.setTextColor(Color.parseColor("#000000"));
         ac_seek_bar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-                String text = "Current temperature: "+(i+16)+" 'C";
+                String text = "Current temperature: " + (i + 16) + " 'C";
                 temp_text.setText(text);
-                if(i < 4)
-                {
+                if (i < 4) {
                     temp_text.setBackgroundColor(Color.parseColor("#0288d1"));
                     ac_seek_bar.setBackgroundColor(Color.parseColor("#0288d1"));
-                }
-                else if(i >= 4 && i < 8)
-                {
+                } else if (i >= 4 && i < 8) {
                     temp_text.setBackgroundColor(Color.parseColor("#26c6da"));
                     ac_seek_bar.setBackgroundColor(Color.parseColor("#26c6da"));
-                }
-                else if(i >= 8 && i < 14)
-                {
+                } else if (i >= 8 && i < 14) {
                     temp_text.setBackgroundColor(Color.parseColor("#b2ebf2"));
                     ac_seek_bar.setBackgroundColor(Color.parseColor("#b2ebf2"));
-                }
-                else
-                {
+                } else {
                     temp_text.setBackgroundColor(Color.parseColor("#ff9e80"));
                     ac_seek_bar.setBackgroundColor(Color.parseColor("#ff9e80"));
                 }
@@ -75,73 +70,63 @@ public class BedroomActivity extends AppCompatActivity {
         });
 
 
-
     }
 
-
-
     public void switchTrigger(View v) throws Exception {
-        Switch s = (Switch)v;
-        if(s.getId() == R.id.master_switch)
-        {
-            if(master.isChecked())
-            {
+        Switch s = (Switch) v;
+        if (s.getId() == R.id.master_switch) {
+            if (master.isChecked()) {
                 lights.setChecked(true);
                 tv.setChecked(true);
                 ac.setChecked(true);
-                s=(Switch)findViewById(R.id.ac_switch);
-                switchCount=3;
-            }
-            else{
+                s = (Switch) findViewById(R.id.ac_switch);
+                switchCount = 3;
+            } else {
 
                 lights.setChecked(false);
                 tv.setChecked(false);
                 ac.setChecked(false);
-                s=(Switch)findViewById(R.id.ac_switch);
-                switchCount=0;
+                s = (Switch) findViewById(R.id.ac_switch);
+                switchCount = 0;
             }
-        }
-        else if(s.getId() == R.id.fans_switch)
-        {
-            ImageView imageView = (ImageView) findViewById(R.id.fan_image);
-            float fromRotation = 0;
-            float toRotation = fromRotation += 300;
-
-            final RotateAnimation rotateAnim = new RotateAnimation(
-                    fromRotation, toRotation, imageView.getWidth()/2, imageView.getHeight()/2);
-
-            rotateAnim.setDuration(1000); // Use 0 ms to rotate instantly
-            rotateAnim.setFillAfter(true); // Must be true or the animation will reset
-            for (int i = 0; i <100; i++)
-                imageView.startAnimation(rotateAnim);
-        }
-        else {
-            if(s.isChecked())
+        } else {
+            if (s.isChecked())
                 switchCount++;
             else
                 switchCount--;
-            if(switchCount==3) {
+            if (switchCount == 3) {
                 master.setChecked(true);
                 //switchTrigger(master);
-            }
-            else
-            {
+            } else {
                 master.setChecked(false);
             }
         }
+        if (tv.isChecked()) {
+            tv_icon.setImageResource(R.drawable.led_tv_on);
+        } else {
+            tv_icon.setImageResource(R.drawable.led_tv);
+        }
+        if (ac.isChecked()) {
+            ac_icon.setImageResource(R.drawable.ac_on);
+        } else {
+            ac_icon.setImageResource(R.drawable.ac);
+        }
+        if (lights.isChecked()) {
+            light_icon.setImageResource(R.drawable.light_on);
+        } else {
+            light_icon.setImageResource(R.drawable.light);
+        }
 
-        if(s.getId()==R.id.ac_switch)
-        {
-            if(s.isChecked()) {
+        if (s.getId() == R.id.ac_switch) {
+            if (s.isChecked()) {
                 ac_seek_bar.setVisibility(View.VISIBLE);
                 temp_text.setVisibility(View.VISIBLE);
-            }
-            else {
+            } else {
                 ac_seek_bar.setVisibility(View.GONE);
                 temp_text.setVisibility(View.GONE);
             }
         }
-        Log.d("CaptiosX",switchCount.toString());
+        Log.d("CaptiosX", switchCount.toString());
 
     }
 
--- app/src/main/java/com/example/mitrjain/homeautomation/ControlAxctivity.java
+++ app/src/main/java/com/example/mitrjain/homeautomation/ControlAxctivity.java
@@ -1,6 +1,7 @@
 package com.example.mitrjain.homeautomation;
 
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -12,60 +13,66 @@ import android.widget.Switch;
  */
 
 public class ControlAxctivity extends AppCompatActivity {
-   Switch master,fans,lights,tv;
-    Integer switchCount=0;
+    Switch master, fans, lights, tv;
+    Integer switchCount = 0;
+    ImageView tv_icon, light_icon, fan_icon;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_control);
-        master = (Switch)findViewById(R.id.master_switch);
-        fans = (Switch)findViewById(R.id.fans_switch);
-        lights = (Switch)findViewById(R.id.lights_switch);
-        tv = (Switch)findViewById(R.id.tv_switch);
+        master = (Switch) findViewById(R.id.master_switch);
+        fans = (Switch) findViewById(R.id.fans_switch);
+        lights = (Switch) findViewById(R.id.lights_switch);
+        tv = (Switch) findViewById(R.id.tv_switch);
+        tv_icon = (ImageView) findViewById(R.id.tv_image);
+        light_icon = (ImageView) findViewById(R.id.lights_image);
+        fan_icon = (ImageView) findViewById(R.id.fan_image);
     }
 
     public void switchTrigger(View v) throws Exception {
-        Switch s = (Switch)v;
-        if(s.getId() == R.id.master_switch)
-        {
-            if(master.isChecked())
-            {
+        Switch s = (Switch) v;
+        if (s.getId() == R.id.master_switch) {
+            if (master.isChecked()) {
                 fans.setChecked(true);
                 lights.setChecked(true);
                 tv.setChecked(true);
-                switchCount=3;
-            }
-            else{
+                switchCount = 3;
+            } else {
                 fans.setChecked(false);
                 lights.setChecked(false);
                 tv.setChecked(false);
-                switchCount=0;
+                switchCount = 0;
             }
-        }
-        else if(s.getId() == R.id.fans_switch)
-        {
-            ImageView imageView = (ImageView) findViewById(R.id.fan_image);
-            for(int i = 0; i < 10; i++)
-            {
-                imageView.setRotation(imageView.getRotation() + 45);
-                Thread.sleep(100);
-            }
-        }
-        else {
-            if(s.isChecked())
+        } else {
+            if (s.isChecked())
                 switchCount++;
             else
                 switchCount--;
-            if(switchCount==3) {
+            if (switchCount == 3) {
                 master.setChecked(true);
                 //switchTrigger(master);
-            }
-            else
-            {
+            } else {
                 master.setChecked(false);
             }
         }
-        Log.d("CaptiosX",switchCount.toString());
+        if (tv.isChecked()) {
+            tv_icon.setImageResource(R.drawable.led_tv_on);
+        } else {
+            tv_icon.setImageResource(R.drawable.led_tv);
+        }
+        if (fans.isChecked()) {
+            fan_icon.setImageResource(R.drawable.fan_on);
+        } else {
+            fan_icon.setImageResource(R.drawable.fan);
+        }
+        if (lights.isChecked()) {
+            light_icon.setImageResource(R.drawable.light_on);
+        } else {
+            light_icon.setImageResource(R.drawable.light);
+        }
+
+        Log.d("CaptiosX", switchCount.toString());
 
     }
 
